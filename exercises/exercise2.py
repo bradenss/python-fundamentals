26694de8bc67781593e4caeefe4c5a5099d37747
bschnieders <bschnieders@WSUtech.edu>
2022-03-03 09:21:23 -0600

    CAC-102-2 exercise2

---
+++ exercises/exercise2.py
@@ -0,0 +1,32 @@
+def simple(title):
+    print(f"Different objects that I see are a {title.title()}.")
+simple('cat')
+simple('fan')
+simple('laptop')
+simple('water Bottle')
+simple('tree')
+
+# This completes question 1
+
+value = "The sun rises from the east"
+print(value)
+value = "The sun does not rise from the west"
+print(value)
+value = "Spring is fastly approaching"
+print(value)
+value = "April Showers brings May Flowers"
+print(value)
+
+# This completes question 2
+
+def message(arg1):
+    print(arg1)
+
+
+
+message('The cat was let out of the bag')
+
+
+# This completes question 3
+
+
