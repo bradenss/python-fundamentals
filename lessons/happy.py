920343a87318299cc3f5c9428845395e1846de72
bschnieders <bschnieders@WSUtech.edu>
2022-03-16 10:58:12 -0500

    CAC-102-13 Modules & Packages lesson13

---
+++ lessons/happy.py
@@ -0,0 +1,7 @@
+# lesson 13 Modules & Packages
+# Basic file to demonstrate importing
+def express(is_happy=True):
+    if is_happy:
+        return 'I am happy'
+    else:
+        return 'I am not happy'
